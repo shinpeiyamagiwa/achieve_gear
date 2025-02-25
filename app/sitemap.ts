e71f8cbd141e9c6852e06455f4cd4f7dcc70700a
shinpeiyamagiwa <wavacon@gmail.com>
2025-02-25 12:10:38 +0900

    sitemap修正

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,19 +1,18 @@
-import type { MetadataRoute } from "next"
+import type { MetadataRoute } from "next";
 
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: "https://achieve-gear.vercel.app",
+      url: "https://achievegear.jp/",
       lastModified: new Date(),
       changeFrequency: "weekly",
       priority: 1,
     },
     {
-      url: "https://achieve-gear.vercel.app/consultation",
+      url: "https://achievegear.jp/consultation",
       lastModified: new Date(),
       changeFrequency: "weekly",
       priority: 0.8,
     },
-  ]
+  ];
 }
-
